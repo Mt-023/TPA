5c449234b7630d248a2601b1aa295c225b8c7cca
Mt-023 <matheusdias10117@gmail.com>
2024-09-24 21:54:31 -0300

    Add files via upload

---
+++ exercicio 12-08/estatistica.java
@@ -0,0 +1,26 @@
+
+public class estatistica {
+    public static void main (String[] args) {
+    	Scanner ler = new Scanner(System.in);
+    	double mb, b, r, i;
+    	int t;
+    	
+    	System.out.println("entre com total de alunos");
+    	
+    	System.out.println("quantidade de alunos que tirou mb");
+    	
+    	System.out.println("quantidade de alunos que tirou m");
+    	
+    	System.out.println("quantidade de alunos que tirou r");
+    	
+    	System.out.println("quantidade de alunos que tirou i");
+    	
+    	t = (mb+b+r+i);
+    	System.out.println("sua porcentagem é:");
+    	
+    	case "mb": 
+    		mb: mb/100t;
+    		
+    		
+    }
+}
