400ad63772fbae7977a7a35b548b767c2fbbee79
Mt-023 <matheusdias10117@gmail.com>
2024-09-24 21:08:06 -0300

    Add files via upload

---
+++ atividade 26-08/pares.java
@@ -0,0 +1,12 @@
+
+public class pares {
+
+	public static void main(String[] args) {
+		int i = 0;
+        while (i<201) {
+        	System.out.println(i);
+        	i = i+2;
+        }
+	}
+
+}
---
+++ atividade 26-08/soma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class soma {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int i=1,s;
+		System.out.println("leia soma");
+		
+
+	}
+
+}
